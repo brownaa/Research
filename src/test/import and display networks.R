0624435c3e9856236a3ef638c6d9c797ee6a2a59
Aaron Brown <brownaa@plj.wireless.jhu.edu>
2013-06-19 12:44:38 -0400

    cleaned up "import and display networks.R"

--- src/test/import and display networks.R
+++ src/test/import and display networks.R
@@ -1,19 +1,19 @@
 library(igraph)
 
-oldwd <- getwd()
+## ~~~~~ Update the working directory to the location of my networks ~~~~~
+oldwd <- getwd()		#preserve old working directory to permit switching
 setwd("/Users/brownaa/Dropbox/Documents/School/2013/Research/R Proj/data/networks/")
-EL <- as.matrix(read.csv("./toy2/edgelist.csv", header=TRUE))
-plot(graph.edgelist(EL[,1:2]))
+## ~~~~~
 
 EL <- read.csv("./toy2/edgelist.csv", header=TRUE)
 nodes <- read.csv("./toy2/node.attr.csv")
 
 g <- graph.data.frame(EL,directed=TRUE, vertices=nodes)
-
-get.data.frame(g, what="edges")
-get.data.frame(g, what="vertices")
-
 plot(g,layout=layout.kamada.kawai, edge.arrow.size=E(g)$Constraint/30, 
 		 edge.width=E(g)$Constraint/5)
 
+# get.data.frame(g, what="edges")				# returns the edges    dataframe with attributes
+# get.data.frame(g, what="vertices")		# returns the vertecis dataframe with attributes
+
+## ~~~~~ return the working directory to its original location
 setwd(oldwd)
